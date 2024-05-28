7d40edc3dcac2b426d17f7a6a930be9e205b8ce2
dekbanbanproject <dekbanbanproject@gmail.com>
2024-05-28 08:12:38 +0700

    select

--- lib/screens/admin/fire/firechang.dart
+++ lib/screens/admin/fire/firechang.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pkoffice/models/firelistmodel.dart';
+import 'package:http/http.dart' as http;
+import 'package:pkoffice/utility/my_constant.dart';
 
 class FireChang extends StatefulWidget {
   final FireListmodel fireModelchang;
@@ -14,7 +20,7 @@ class _FireChangState extends State<FireChang> {
   final formKey = GlobalKey<FormState>();
   late FireListmodel _fireModelchang;
   String? fire_id, fire_num, fire_name, fire_size, fire_color, fire_location;
-    List<FireListmodel> fireModel = [];
+  List<FireListmodel> fireModel = [];
   @override
   void initState() {
     super.initState();
@@ -25,9 +31,31 @@ class _FireChangState extends State<FireChang> {
     fire_size = _fireModelchang.fire_size;
     fire_color = _fireModelchang.fire_color;
     fire_location = _fireModelchang.fire_location;
+
+    // getfire();
   }
 
-  // Future<>
+  Future<List<FireListmodel>> getfire() async {
+    try {
+      final response = await http.get(Uri.parse(
+          '${MyConstant.domain}/pkoffice/api/getfirechang.php?isAdd=true'));
+      final body = json.decode(response.body) as List;
+      if (response.statusCode == 200) {
+        return body.map((e) {
+          final map = e as Map<String, dynamic>;
+          return FireListmodel(
+              fire_id: map['fire_id'],
+              fire_num: map['fire_num'],
+              fire_name: map['fire_name']);
+        }).toList();
+      }
+    } on SocketException {
+      throw Exception('Failed Network Connect Error');
+    }
+    throw Exception('Failed to get Data error.');
+  }
+
+  var selectedValue;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +74,40 @@ class _FireChangState extends State<FireChang> {
                   color: Colors.white),
             ),
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            FutureBuilder<List<FireListmodel>>(
+              future: getfire(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return DropdownButton(
+                    value: selectedValue,
+                    dropdownColor: Colors.white,
+                    isExpanded: true,
+                    hint: const Text('Select Fire Item'),
+                    items: snapshot.data!.map((e) {
+                      return DropdownMenuItem(
+                          value: e.fire_id.toString(),
+                          child: Text(e.fire_num.toString()));
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('error:${snapshot.error}');
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            )
+          ],
         ),
       ),
     );
